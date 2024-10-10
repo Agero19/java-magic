ddd185c1c698b0b4201082ab1121a92d4daae10f
Agero19 <pmotulko@gmail.com>
2024-10-10 23:50:36 +0200

    Types, arrays, loops

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        System.out.println("Oh, you're a magician, friend! Welcome to Hogwarts!");
+
+        String dumbledore = "Dumbledore";
+        String mcgonagall = "McGonagall";
+        String snape = "Snape";
+        String hugrid = "Hugrid";
+        String lupine = "Lupine";
+        String[] teachers = {
+                dumbledore,
+                mcgonagall,
+                snape,
+                hugrid,
+                lupine
+        };
+        String intro = "School teachers:";
+        System.out.println(intro);
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i + ". " + teachers[i]);
+        }
     }
 
 }
---
+++ src/Spell.java
@@ -0,0 +1,5 @@
+public class Spell {
+    public static void main(String[] args) {
+        System.out.println("Casting spell!");
+    }
+}
