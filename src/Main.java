c403442c35646e57407694aff088e3be40847e83
Agero19 <pmotulko@gmail.com>
2024-10-16 21:42:44 +0200

    HW done for l3

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Oh, you're a magician, friend! Welcome to Hogwarts!");
@@ -17,8 +19,25 @@ public class Main {
         String intro = "School teachers:";
         System.out.println(intro);
         for (int i = 0; i < 5; i++) {
-            System.out.println(i + ". " + teachers[i]);
+            System.out.println(i + 1 + ". " + teachers[i]);
         }
+
+        int grade = new Random().nextInt(1,10);
+        int grade2 = new Random().nextInt(1,10);
+
+        boolean result = grade >= 5 && grade2 >= 5;
+        System.out.println("Grade: " + grade + ", Grade2: " + grade2);
+        System.out.println(result ? "Test passed" : "Test failed");
+        float avg =  (float) (grade + grade2) / 2;
+        System.out.println("Average grade: " + avg);
+
+        float n = 4199.F;
+        float[] remainders = {n / 13, n / 14,n / 15, n / 16, n / 17, n / 18, n / 19};
+        float sum = 0;
+        for (float remainder : remainders) {
+            sum += remainder;
+        }
+        System.out.println("Sum of remainders: " +  sum);
     }
 
 }
